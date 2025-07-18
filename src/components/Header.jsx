acb06f45a3c9b6699a63bfcceecbabc2b76c5418
KoloinaElie <koloinaelie25@gmail.com>
2025-07-18 17:05:42 +0300

    ref(pages): Make all pages visible again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => {
       <h1 className="profile-title">Koloina Elie.</h1>
       <h2 className="profile-subtitle">Frontend Developer</h2>
       <p className="profile-description">
-        I build accessible, pixel-perfect digital experiences for the web.
+        I build accessible, pixel-perfect digital 
+        <br /> experiences for the web.
       </p>
     </div>
   );
